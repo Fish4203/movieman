9d1e93a204882b4cbceabb5ad0471945dcf41432
Fish4203 <fishs4203@gmail.com>
2023-08-06 11:18:18 +1000

    basic tuorial

--- gin-mongo-api/main.go
+++ gin-mongo-api/main.go
@@ -1,15 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "gin-mongo-api/configs"
+    "gin-mongo-api/routes"
+    "github.com/gin-gonic/gin"
+)
 
 func main() {
         router := gin.Default()
 
-        router.GET("/", func(c *gin.Context) {
-                c.JSON(200, gin.H{
-                        "data": "Hello from Gin-gonic & mongoDB",
-                })
-        })
+        configs.ConnectDB()
+
+        routes.UserRoute(router)
 
         router.Run("localhost:3000")
 }
