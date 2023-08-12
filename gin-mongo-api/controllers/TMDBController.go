866913e9cfca7834bf8f38754ce01e8268f81142
Fish4203 <fishs4203@gmail.com>
2023-08-12 13:57:15 +1000

    get requests to TMDB

--- gin-mongo-api/controllers/TMDBController.go
+++ gin-mongo-api/controllers/TMDBController.go
@@ -6,7 +6,11 @@ import (
     // "gin-mongo-api/models"
     // "gin-mongo-api/responses"
     // "gin-mongo-api/middleware"
-    "net/http"
+    // "fmt"
+	"net/http"
+	"io"
+    "encoding/json"
+    "os"
     // "time"
 
     "github.com/gin-gonic/gin"
@@ -17,13 +21,50 @@ import (
     // "golang.org/x/crypto/bcrypt"
 )
 
+func getTMDB(params string) (map[string]interface{}, error) {
+    var jsonResponse map[string]interface{}
+
+    req, err := http.NewRequest(http.MethodGet, "https://api.themoviedb.org/3/" + params, nil)
+    if err != nil {
+        return jsonResponse, err
+    }
+
+    req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", "Bearer " + os.Getenv("TMDB"))
+
+
+    res, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return jsonResponse, err
+    }
+
+    defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+    if err != nil {
+        return jsonResponse, err
+    }
+
+    err = json.Unmarshal(body, &jsonResponse)
+    if err != nil {
+        return jsonResponse, err
+    }
+
+    return jsonResponse, nil
+}
 
 func TMDBTest() gin.HandlerFunc {
     return func(c *gin.Context) {
         // ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
         // defer cancel()
 
-        c.JSON(http.StatusCreated, map[string]interface{}{"result": "success"})
+        json, err := getTMDB("authentication")
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+            return
+        }
+
+
+        c.JSON(http.StatusOK, json)
     }
 }
 
