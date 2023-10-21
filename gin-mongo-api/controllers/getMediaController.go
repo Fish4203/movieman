c43ba1b74b17fba8f19ebae2ef64e321b30fad85
Fish4203 <fishs4203@gmail.com>
2023-10-21 17:37:45 +1100

    re did media

--- gin-mongo-api/controllers/getMediaController.go
+++ gin-mongo-api/controllers/getMediaController.go
@@ -1,149 +1,210 @@
 package controllers
 
 import (
-    // "context"
-    // "gin-mongo-api/configs"
-    "gin-mongo-api/models"
-    // "gin-mongo-api/responses"
-    // "gin-mongo-api/middleware"
-    // "fmt"
+	"gin-mongo-api/models"
 	"net/http"
-	// "io"
-    // "encoding/json"
-    // "os"
-    // "strings"
-    // "compress/gzip"
-    // "time"
-    "strconv"
+	"strings"
 
-    "github.com/gin-gonic/gin"
-    // "github.com/go-playground/validator/v10"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    // "go.mongodb.org/mongo-driver/mongo"
-    // "golang.org/x/crypto/bcrypt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-
 func GetMediaSearch() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Query("q")
+	return func(c *gin.Context) {
+		query := c.Query("q")
+		types := c.Query("types")
+		var out = make(map[string]interface{})
 
-        movies, err := models.FindMovie(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-        persons, err := models.FindPerson(bson.D{{"name", bson.D{{"$regex", query}, {"$options", "i"}}}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-        shows, err := models.FindShow(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-        episodes, err := models.FindShowEpisode(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+		if strings.ContainsAny(types, "m") {
+			movies, err := models.FindMovie(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["movies"] = movies
+		}
 
-        c.JSON(http.StatusOK, map[string]interface{}{"movies": movies, "shows": shows, "episodes": episodes, "people": persons})
-    }
+		if strings.ContainsAny(types, "s") {
+			shows, err := models.FindShow(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["shows"] = shows
+		}
+
+		if strings.ContainsAny(types, "e") {
+			episodes, err := models.FindShowEpisode(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["episodes"] = episodes
+		}
+
+		if strings.ContainsAny(types, "p") {
+			persons, err := models.FindPerson(bson.D{{"name", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["people"] = persons
+		}
+
+		if strings.ContainsAny(types, "b") {
+			books, err := models.FindBook(bson.D{{Key: "title", Value: bson.D{{Key: "$regex", Value: query}, {Key: "$options", Value: "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["books"] = books
+		}
+
+		if strings.ContainsAny(types, "v") {
+			games, err := models.FindGame(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["games"] = games
+		}
+
+		if strings.ContainsAny(types, "g") {
+			groups, err := models.FindGroup(bson.D{{"title", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["groups"] = groups
+		}
+
+		if strings.ContainsAny(types, "m") {
+			companies, err := models.FindCompany(bson.D{{"name", bson.D{{"$regex", query}, {"$options", "i"}}}})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+				return
+			}
+			out["companies"] = companies
+		}
+
+		c.JSON(http.StatusOK, out)
+	}
 }
 
 func MovieDetails() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Param("movieId")
-        objId, _ := primitive.ObjectIDFromHex(query)
+	return func(c *gin.Context) {
+		query := c.Param("movieId")
+		objId, _ := primitive.ObjectIDFromHex(query)
 
-        movies, err := models.FindMovie(bson.D{{"_id", objId}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+		movies, err := models.FindMovie(bson.D{{"_id", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
 
-        c.JSON(http.StatusOK, map[string]interface{}{"movies": movies})
-    }
-}
-
-func PersonDetails() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Param("personId")
-        objId, _ := primitive.ObjectIDFromHex(query)
-
-        results, err := models.FindPerson(bson.D{{"_id", objId}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-
-        c.JSON(http.StatusOK, map[string]interface{}{"people": results})
-    }
+		c.JSON(http.StatusOK, map[string]interface{}{"movies": movies})
+	}
 }
 
 func ShowDetails() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Param("showId")
-        objId, _ := primitive.ObjectIDFromHex(query)
+	return func(c *gin.Context) {
+		query := c.Param("showId")
+		objId, _ := primitive.ObjectIDFromHex(query)
 
-        results, err := models.FindShow(bson.D{{"_id", objId}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+		show, err := models.FindShow(bson.D{{"_id", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
 
-        c.JSON(http.StatusOK, map[string]interface{}{"shows": results})
-    }
+		seasons, err := models.FindShowSeason(bson.D{{"showId", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
+
+		episodes, err := models.FindShowEpisode(bson.D{{"showId", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, map[string]interface{}{"shows": show, "seasons": seasons, "episodes": episodes})
+	}
 }
 
-func ShowSeasonDetails() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Param("showId")
-        season, err := strconv.Atoi(c.Param("seasonId"))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+func BookDetails() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		query := c.Param("Id")
+		objId, _ := primitive.ObjectIDFromHex(query)
 
-        objId, _ := primitive.ObjectIDFromHex(query)
+		obj, err := models.FindBook(bson.D{{Key: "_id", Value: objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
 
-        results, err := models.FindShowSeason(bson.D{{"showId", objId}, {"seasonId", season}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-
-        c.JSON(http.StatusOK, map[string]interface{}{"season": results})
-    }
+		c.JSON(http.StatusOK, map[string]interface{}{"books": obj})
+	}
 }
 
+func GameDetails() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		query := c.Param("Id")
+		objId, _ := primitive.ObjectIDFromHex(query)
 
-func ShowEpisodeDetails() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        query := c.Param("showId")
-        season, err := strconv.Atoi(c.Param("seasonId"))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
-        episode, err := strconv.Atoi(c.Param("episodeId"))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+		obj, err := models.FindGame(bson.D{{Key: "_id", Value: objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
 
+		c.JSON(http.StatusOK, map[string]interface{}{"games": obj})
+	}
+}
 
-        objId, _ := primitive.ObjectIDFromHex(query)
+func PersonDetails() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		query := c.Param("personId")
+		objId, _ := primitive.ObjectIDFromHex(query)
 
-        results, err := models.FindShowEpisode(bson.D{{"showId", objId}, {"seasonId", season}, {"episodeId", episode}})
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-            return
-        }
+		results, err := models.FindPerson(bson.D{{"_id", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
 
-        c.JSON(http.StatusOK, map[string]interface{}{"episode": results})
-    }
+		c.JSON(http.StatusOK, map[string]interface{}{"people": results})
+	}
+}
+
+func GroupDetails() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		query := c.Param("Id")
+		objId, _ := primitive.ObjectIDFromHex(query)
+
+		obj, err := models.FindGroup(bson.D{{"_id", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, map[string]interface{}{"groups": obj})
+	}
+}
+
+func CompanyDetails() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		query := c.Param("Id")
+		objId, _ := primitive.ObjectIDFromHex(query)
+
+		obj, err := models.FindCompany(bson.D{{"_id", objId}})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, map[string]interface{}{"companies": obj})
+	}
 }
--- gin-mongo-api/routes/getMediaRoute.go
+++ gin-mongo-api/routes/getMediaRoute.go
@@ -1,17 +1,20 @@
 package routes
 
 import (
-    "gin-mongo-api/controllers"
-    "github.com/gin-gonic/gin"
+	"gin-mongo-api/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
-func GetMediaRoute(router *gin.Engine)  {
-    //detailes
-    router.GET("/media/movie/:movieId", controllers.MovieDetails())
-    router.GET("/media/show/:showId/:seasonId/:episodeId", controllers.ShowEpisodeDetails())
-    router.GET("/media/show/:showId/:seasonId", controllers.ShowSeasonDetails())
-    router.GET("/media/show/:showId", controllers.ShowDetails())
-    router.GET("/media/person/:personId", controllers.PersonDetails())
-    // serch
-    router.GET("/media/search", controllers.GetMediaSearch())
+func GetMediaRoute(router *gin.Engine) {
+	//detailes
+	router.GET("/media/movie/:movieId", controllers.MovieDetails())
+	router.GET("/media/show/:showId", controllers.ShowDetails())
+	router.GET("/media/book/:Id", controllers.BookDetails())
+	router.GET("/media/game/:Id", controllers.GameDetails())
+	router.GET("/media/person/:personId", controllers.PersonDetails())
+	router.GET("/media/group/:Id", controllers.GroupDetails())
+	router.GET("/media/company/:Id", controllers.CompanyDetails())
+	// serch
+	router.GET("/media/search", controllers.GetMediaSearch())
 }
