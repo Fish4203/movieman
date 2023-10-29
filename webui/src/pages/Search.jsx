32a4bed6deeb828c29a0944e3f916eff7c12a0f8
Fish4203 <fishs4203@gmail.com>
2023-10-30 00:29:32 +1100

    update frontend search to talk to services and fixed formating

--- webui/src/pages/Search.jsx
+++ webui/src/pages/Search.jsx
@@ -1,14 +1,9 @@
-import { Button, Container, Card, Row, Col, Form, ToggleButton } from 'react-bootstrap';
+import { Button, Container, Card, Row, Col, Form, ToggleButton, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Result} from '../components/Results';
 // import "./styles.css";
 
-const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
-];
 
 function Search() {
   const [movies, setMovies] = useState([]);
@@ -29,20 +24,18 @@ function Search() {
   const [peopleBool, setPeopleBool] = useState(true);
   const [companieBool, setCompanieBool] = useState(true);
   const [groupBool, setGroupBool] = useState(true);
-  const [provBool, setProvBool] = useState(initialList);
+  const [provBool, setProvBool] = useState([]);
   
   const [status, setStatus] = useState("");
-  const [provs, setProvs] = useState([])
 
 
-  function handleIncrementClick(index, val) {
-    setProvBool(provBool.map(artwork => {
-      if (artwork.id === index) {
-        // Create a *new* object with changes
-        return { ...artwork, seen: val };
+  function handleProvToggle(index, val) {
+    setProvBool(provBool.map(provbooli => {
+      if (provbooli.id === index) {
+        return { ...provbooli, enabled: val };
       } else {
         // No changes
-        return artwork;
+        return provbooli;
       }
     }));
   }
@@ -52,7 +45,13 @@ function Search() {
     .then(function (response) {
       // handle success
       // console.log(response.data)
-      setProvs(response.data);
+      let data = response.data;
+      data.forEach(element => {
+        element["enabled"] = true;
+      });
+
+      setProvBool(data);
+
     })
     .catch(function (error) {
       // handle error
@@ -61,33 +60,87 @@ function Search() {
     })
   }, [])
 
-  async function searchApis(url) {
-    
-    axios.get(url + '/search?q=' + query + '&types=' + types)
-    .then(function (response) {
-      setStatus("Sucsess")
-    })
-    .catch(function (error) {
-      setStatus(error);
-      console.log(error);
+  function getTypes() {
+    let types = "";
+    if (movieBool) {
+      types += "m";
+    }
+    if (showBool) {
+      types += "s";
+    }
+    if (episodeBool) {
+      types += "e";
+    }
+    if (bookBool) {
+      types += "b";
+    }
+    if (gameBool) {
+      types += "v";
+    }
+    if (peopleBool) {
+      types += "p";
+    }
+    if (companieBool) {
+      types += "c";
+    }
+    if (groupBool) {
+      types += "g";
+    }
+    return types;
+  }
+
+
+  function searchApis() {
+    let types = getTypes();
+
+    let urls = [];
+    provBool.map((prov) => {
+      urls.push(prov.domain + '/search?q=' + query + '&types=' + types);
     });
+
+    const requests = urls.map((url) => axios.get(url));
+
+    axios.all(requests).then(axios.spread((...responses) => {
+      responses.forEach((resp) => {
+        console.log(resp);
+      });
+    }));
+    
   }
 
   function getSearch() {
+    let types = getTypes();
 
     axios.get(import.meta.env.VITE_BACKEND + '/media/search?q=' + query + '&types=' + types)
     .then(function (response) {
       // handle success
       // console.log(response.data)
-      setMovies(response.data.movies);
-      setShows(response.data.shows);
-      setEpisodes(response.data.episodes);
-      setBooks(response.data.books);
-      setGames(response.data.Games);
-      setPeople(response.data.people);
-      setCompanies(response.data.companies);
-      setGroups(response.data.groups);
-      // setData(response.data);
+      if (response.data.movies != null) {
+        setMovies(response.data.movies);
+      }
+
+      if (response.data.shows != null) {
+        setShows(response.data.shows);
+      }
+      if (response.data.episodes != null) {
+        setEpisodes(response.data.episodes);
+      }
+      if (response.data.books != null) {
+        setBooks(response.data.books);
+      }
+      if (response.data.Games != null) {
+        setGames(response.data.Games);
+      }
+      if (response.data.people != null) {
+        setPeople(response.data.people);
+      }
+      if (response.data.companies != null) {
+        setCompanies(response.data.companies);
+      }
+      if (response.data.groups != null) {
+        setGroups(response.data.groups);
+      }
+        // setData(response.data);
     })
     .catch(function (error) {
       // handle error
@@ -111,113 +164,134 @@ function Search() {
                     value={query}
                     onKeyPress={(event) => {if (event.charCode===13) {getSearch()}}}
                 />
-                <Button onClick={getSearch}>Search</Button>
+                <Button className='ms-1' onClick={searchApis}>Full Search</Button>
+                <Button className='ms-1' onClick={getSearch}>quick Search</Button>
             </div>
-
-                <br />
-
-                
-                <ToggleButton
-                  className="mb-2"
-                  id='movieBool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={movieBool}
-                  onChange={(e) => setMovieBool(e.currentTarget.checked)}
-                >
-                  Movie
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='showbool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={showBool}
-                  onChange={(e) => setShowBool(e.currentTarget.checked)}
-                >
-                  Show
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='episodebool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={episodeBool}
-                  onChange={(e) => setEpisodeBool(e.currentTarget.checked)}
-                >
-                  Episode
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='bookbool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={bookBool}
-                  onChange={(e) => setBookBool(e.currentTarget.checked)}
-                >
-                  Book
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='gamebool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={gameBool}
-                  onChange={(e) => setGameBool(e.currentTarget.checked)}
-                >
-                  Game
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='peoplebool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={peopleBool}
-                  onChange={(e) => setPeopleBool(e.currentTarget.checked)}
-                >
-                  People
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='companybool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={companieBool}
-                  onChange={(e) => setCompanieBool(e.currentTarget.checked)}
-                >
-                  Company
-                </ToggleButton>
-                <ToggleButton
-                  className="mb-2"
-                  id='groupbool'
-                  type="checkbox"
-                  variant="outline-success"
-                  checked={groupBool}
-                  onChange={(e) => setGroupBool(e.currentTarget.checked)}
-                >
-                  Collections
-                </ToggleButton>
 
             <br />
-            <div>
-              {provBool.map(post => (
-              <ToggleButton
-                className="mb-2"
-                id={post.id}
-                type="checkbox"
-                variant="outline-success"
-                checked={post.seen}
-                onChange={(e) => handleIncrementClick(post.id, e.currentTarget.checked)}
-              >
-                {post.title}
-              </ToggleButton>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Header>Search categories</Card.Header>
+                  <Card.Body>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='movieBool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={movieBool}
+                      onChange={(e) => setMovieBool(e.currentTarget.checked)}
+                    >
+                      Movie
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='showbool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={showBool}
+                      onChange={(e) => setShowBool(e.currentTarget.checked)}
+                    >
+                      Show
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='episodebool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={episodeBool}
+                      onChange={(e) => setEpisodeBool(e.currentTarget.checked)}
+                    >
+                      Episode
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='bookbool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={bookBool}
+                      onChange={(e) => setBookBool(e.currentTarget.checked)}
+                    >
+                      Book
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='gamebool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={gameBool}
+                      onChange={(e) => setGameBool(e.currentTarget.checked)}
+                    >
+                      Game
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='peoplebool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={peopleBool}
+                      onChange={(e) => setPeopleBool(e.currentTarget.checked)}
+                    >
+                      People
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='companybool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={companieBool}
+                      onChange={(e) => setCompanieBool(e.currentTarget.checked)}
+                    >
+                      Company
+                    </ToggleButton>
+                    <ToggleButton
+                      className="mb-2 ms-1"
+                      id='groupbool'
+                      type="checkbox"
+                      variant="outline-info"
+                      checked={groupBool}
+                      onChange={(e) => setGroupBool(e.currentTarget.checked)}
+                    >
+                      Collections
+                    </ToggleButton>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">Click to select/unselect to limit what is being searched for</Card.Footer>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <Card.Header>External databases</Card.Header>
+                  <Card.Body>
+                    {provBool.map(post => (
+                      <OverlayTrigger
+                      placement="right"
+                      overlay={
+                        <Tooltip id={`tooltip-right`}>
+                          {post.types}
+                        </Tooltip>
+                      }
+                    >
+                      <ToggleButton
+                        className="mb-2 ms-1"
+                        id={post.id}
+                        type="checkbox"
+                        variant="outline-info"
+                        checked={post.enabled}
+                        onChange={(e) => handleProvToggle(post.id, e.currentTarget.checked)}
+                      >
+                        {post.name}
+                      </ToggleButton>
+                    </OverlayTrigger>
 
-              ))}
-
-            </div>
-            <h2>provs</h2><Row>
-            {provs.map(post => ( <p>{post.name}</p>))}
+                    ))}
+                  </Card.Body>
+                  <Card.Footer className="text-muted">Click to select/unselect to pick the sites to search </Card.Footer>
+                </Card>
+              </Col>
             </Row>
+
+
+            <br />
 
             <h2>Movies</h2><Row>
             {movies.map(post => ( <Result post={post}/>))}
